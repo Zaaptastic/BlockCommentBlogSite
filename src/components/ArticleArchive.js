7a00ae2ec8be7265c24e733ccbd7c5c518135b5f
Bobby Chen <rchentastic@gmail.com>
2022-08-09 22:44:40 -0400

    Integrate with Lambda URL for fetching ArticleArchive

--- src/components/ArticleArchive.js
+++ src/components/ArticleArchive.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { fetchMetadata } from '../utils/AwsFunctions';
 
 
 class ArticleArchive extends React.Component {
     
     constructor(props) {
         super(props);
+        this.state = {data: [] }
+    }
+
+    componentDidMount() {
+        fetch('https://jvnqmq63tuabrbtvppst2r2zkq0ezmbs.lambda-url.us-east-1.on.aws/')
+            .then(res => res.json())
+            .then(json => this.setState({data: json}));
     }
 
     render() {
-        var data = fetchMetadata();
-
         return (
-            <>
-            </>
+            <div>
+            <h1>Articles</h1>
+
+            {this.state.data.map(article => {
+                var url = "/" + article.articleId.S + "/" + article.title.S.replaceAll(' ', '-');
+                return (
+                    <div key={article.articleId.S}>
+                        <h2><a href={url}>{article.title.S}</a></h2>
+                        <i>Published: {article.publicationDate.S}<br />
+                        {article.estimatedReadingTime.N} minute read</i>
+                        <br />
+                        <p>{article.summary.S}</p>
+                    </div>
+                );
+            }
+
+            )}
+            </div>
         );
     }
 }
