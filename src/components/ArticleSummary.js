3ac22b03549d4cfcb2b46cfe1dacbe37881a5fed
Bobby Chen <rchentastic@gmail.com>
2022-08-11 23:01:43 -0400

    Localize publication date

--- src/components/ArticleSummary.js
+++ src/components/ArticleSummary.js
@@ -3,11 +3,13 @@ import React from 'react';
 class ArticleSummary extends React.Component{
     render() {
         var url = "/article/" + this.props.articleId + "/" + this.props.title.replaceAll(' ', '-');
+        var localizedDate = new Date(this.props.publicationDate)
+            .toLocaleDateString('en-US', { dateStyle: 'long' });
 
         return (
             <div>
                 <h2><a href={url}>{this.props.title}</a></h2>
-                <i>Published: {this.props.publicationDate}<br />
+                <i>Published: {localizedDate}<br />
                 {this.props.estimatedReadingTime} minute read</i>
                 <br />
                 <p>{this.props.summary}</p>
