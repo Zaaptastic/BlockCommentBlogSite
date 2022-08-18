c73e01c6bf36c051237ae218f977c33613dfc097
Bobby Chen <rchentastic@gmail.com>
2022-08-17 21:59:24 -0400

    Refactor devo banner to be less intrusive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import '../styles/Header.css'
 import logo from '../assets/logo.png'
+import logoDevo from '../assets/logo_devo.png'
 import React from 'react';
-import DevoBanner from './DevoBanner'
 
-class Header extends React.Component{
+class Header extends React.Component {
     render() {
+        var shouldHideDevoBanner = new URLSearchParams(window.location.search)
+            .get('hideDevoBanner');
+        if ('development' === process.env.NODE_ENV
+            && shouldHideDevoBanner !== '1'
+            && shouldHideDevoBanner !== 'true') {
+                logo = logoDevo;
+        }
         return (
             <div id='header-wrapper'>
-                <DevoBanner />
                 <div id='header'>
                     <a href='/'><img src={logo} id='logo-image'></img></a>
                     <span id='navigation'>
                         <a href='/' className='navigation-link'>Posts</a>
-                        
+
                         <a href='/about' className='navigation-link'>About Me</a>
                     </span>
                 </div>
