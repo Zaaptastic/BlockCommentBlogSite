6f0e3abdff90063298a35edfbd18b2ee51c2d3fc
Bobby Chen <rchentastic@gmail.com>
2022-08-19 20:22:57 -0400

    Style simple 404 page

--- src/components/ErrorContent.js
+++ src/components/ErrorContent.js
@@ -1,9 +1,13 @@
+import '../styles/ErrorContent.css'
 import React from 'react';
 
 class ErrorContent extends React.Component{
     render() {
         return (
-            <h1>404</h1>
+            <div id='error-container'>
+                <p id='error-message'>404</p>
+                <p id='error-link-container'>Get here by mistake? Report a bug <a href='https://github.com/Zaaptastic/BlockCommentBlogSite/issues' target='_blank' rel='noopener noreferrer'>here!</a></p>
+            </div>
         );
     }
 }
