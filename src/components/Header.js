fa732dc0f69daec5eeeb2b10611bafcbf41b13ba
Bobby Chen <rchentastic@gmail.com>
2022-08-22 21:50:10 -0400

    Make mobile header sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import menuCloseIcon from '../assets/menu_close_icon.png'
 import Footer from './Footer'
 import React from 'react';
 
-import { MobileView, BrowserView } from 'react-device-detect';
+import { isMobile, MobileView, BrowserView } from 'react-device-detect';
 
 class Header extends React.Component {
 
@@ -18,18 +18,20 @@ class Header extends React.Component {
             && shouldHideDevoBanner !== 'true') {
             logo = logoDevo;
         }
+
+        var headerId = 'header-desktop';
+        if (isMobile) {
+            headerId = 'header-mobile';
+        }
+
         return (
             <div id='header-wrapper'>
                 <span id='mobile-grey-area' className='hidden-area' onClick={toggleMobileMenu} />
-                <div id='header'>
+                <div id={headerId}>
                     <a href='/'><img src={logo} id='logo-image'></img></a>
                     <MobileView>
                         <img src={menuIcon} onClick={toggleMobileMenu} id='mobile-menu-icon' />
                         <span id='navigation-mobile' className='hidden-area'>
-
-                            <div className='naviation-item-mobile'>
-                                <img src={menuCloseIcon} onClick={toggleMobileMenu} id='mobile-menu-close-icon' />
-                            </div>
 
                             <div className='naviation-item-mobile'>
                                 <a href='/' className='navigation-link'>Posts</a>
