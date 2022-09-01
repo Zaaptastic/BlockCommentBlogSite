ba7b5aca1a91dd1ed8f16518c26d0ac310defba6
Bobby Chen <rchentastic@gmail.com>
2022-09-01 19:37:21 -0400

    Formally wrap rows in desktop ArticleArchive, setting up future styling work

--- src/components/ArticleArchive.js
+++ src/components/ArticleArchive.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ArticleSummary from './ArticleSummary'
 import Loading from './Loading'
 import { fetchArticleMetadata } from '../utils/AwsFunctions'
-import { isMobile } from 'react-device-detect';
+import { isMobile, BrowserView, MobileView } from 'react-device-detect';
 
 
 class ArticleArchive extends React.Component {
@@ -26,45 +26,79 @@ class ArticleArchive extends React.Component {
         var articles = this.state.data;
         var heroArticle = undefined;
         var archiveWrapperId = 'article-archive-wrapper-mobile';
-        
+
+        // Only used on Desktop View for Grid Layout
+        var columnCount = 3;
+        var articleRows = [];
+
         // If desktop, heroArticle does not get populated, resulting in all articles rendering equivalently
         if (!isMobile) {
-            heroArticle = this.state.data.shift();
-            archiveWrapperId = 'article-archive-wrapper-desktop';
+            var archiveWrapperId = 'article-archive-wrapper-desktop';
+            heroArticle = articles.shift();
+            
+            for (let index = 0; index < articles.length; index++) {
+                var rowNumber = Math.floor(index/columnCount);
+                if (articleRows[rowNumber] === undefined) {
+                    articleRows[rowNumber] = [];
+                }
+                articleRows[rowNumber].push(articles[index]);
+            }
         }
-        
+
         return (
             <div id={archiveWrapperId} >
                 {this.state.isPageLoading === true && <Loading />}
-                
+
                 {heroArticle !== undefined && <div id='hero-article-wrapper'>
-                        <ArticleSummary
-                            title={heroArticle.title.S}
-                            articleId={heroArticle.articleId.S}
-                            publicationDate={heroArticle.publicationDate.S}
-                            estimatedReadingTime={heroArticle.estimatedReadingTime.N}
-                            summary={heroArticle.summary.S}
-                            imageUrl={heroArticle.imageUrl.S}
-                            tags={heroArticle.tags.L}
-                            isHero={true}
-                        />
-                    </div>
+                    <ArticleSummary
+                        title={heroArticle.title.S}
+                        articleId={heroArticle.articleId.S}
+                        publicationDate={heroArticle.publicationDate.S}
+                        estimatedReadingTime={heroArticle.estimatedReadingTime.N}
+                        summary={heroArticle.summary.S}
+                        imageUrl={heroArticle.imageUrl.S}
+                        tags={heroArticle.tags.L}
+                        isHero={true}
+                    />
+                </div>
                 }
 
-                <div id='article-history-wrapper'>
-                    {articles.map(article => {
-                        return <div key={article.articleId.S}>
-                            <ArticleSummary
-                                title={article.title.S}
-                                articleId={article.articleId.S}
-                                publicationDate={article.publicationDate.S}
-                                estimatedReadingTime={article.estimatedReadingTime.N}
-                                summary={article.summary.S}
-                                imageUrl={article.imageUrlSmall.S}
-                                tags={article.tags.L}
-                            /></div>
-                    })}
-                </div>
+                <MobileView>
+                    <div id='article-history-wrapper-mobile'>
+                        {articles.map(article => {
+                            return <div key={article.articleId.S}>
+                                <ArticleSummary
+                                    title={article.title.S}
+                                    articleId={article.articleId.S}
+                                    publicationDate={article.publicationDate.S}
+                                    estimatedReadingTime={article.estimatedReadingTime.N}
+                                    summary={article.summary.S}
+                                    imageUrl={article.imageUrlSmall.S}
+                                    tags={article.tags.L}
+                                /></div>
+                        })}
+                    </div>
+                </MobileView>
+
+                <BrowserView>
+                    <div id='article-history-wrapper-desktop'>
+                    {articleRows.map(articleRow => {
+                            return <div key={articleRow} className='article-history-row'>
+                                {articleRow.map(article => {
+                                    return <div key={article.articleId.S}>
+                                    <ArticleSummary
+                                        title={article.title.S}
+                                        articleId={article.articleId.S}
+                                        publicationDate={article.publicationDate.S}
+                                        estimatedReadingTime={article.estimatedReadingTime.N}
+                                        summary={article.summary.S}
+                                        imageUrl={article.imageUrlSmall.S}
+                                        tags={article.tags.L}
+                                    /></div>
+                                })}</div>
+                        })}
+                    </div>
+                </BrowserView>
             </div>
         );
     }
